e7c9d378963285e69ba92809fd4dea02383e192c
Jimbo Freedman <github@jlf.me>
2018-01-10 00:27:11 -0700

    ActionContextIcon Prototype

---
+++ app/components/Action/ActionContextIcon.js
@@ -0,0 +1,21 @@
+/**
+*
+* Action
+*
+*/
+
+import React from 'react';
+
+function ActionContextIcon(props) {
+
+  return (
+    <div>
+
+    </div>);
+}
+
+ActionContextIcon.propTypes = {
+  context: React.PropTypes.object,
+};
+
+export default ActionContextIcon;
