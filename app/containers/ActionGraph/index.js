52496fe984d2f2bbfce7797bda14b81d2c508c2a
Jimbo Freedman <jimbo@project-sapiens.com>
2018-07-25 04:02:45 +0100

    Remove bad ActionGraph

--- app/containers/ActionGraph/index.js
+++ app/containers/ActionGraph/index.js
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { ForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -26,29 +25,7 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
       (<div>Loading</div>)
       :
       (
-        <ForceGraph
-          simulationOptions={{
-            height: 1000,
-            width: 1000,
-            animate: false,
-            strength: {
-              collide: 20,
-            },
-          }}
-          labelAttr="shortDescription"
-          showLabels
-        >
-          {
-            Object.keys(actions.data)
-              .filter((id) => actions.data[id].status === 0)
-              .map((id) => {
-                const action = actions.data[id];
-                const links = action.dependencies.filter((dependencyId) => actions.data[dependencyId].status === 0).map((dependencyId) =>
-                  <ForceGraphLink link={{ source: action.id, target: dependencyId }} />);
-                return links.concat([<ForceGraphNode key={action.id} node={{ ...action, radius: 10 }} fill="red" />]);
-              })
-          }
-        </ForceGraph>
+        <div>Disabled</div>
       );
   }
 }
