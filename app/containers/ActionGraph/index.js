4228c052184bc9374f26402d574dab444985de43
Jimbo Freedman <jimbo@project-sapiens.com>
2018-07-28 06:23:52 +0100

    Lint

--- app/containers/ActionGraph/index.js
+++ app/containers/ActionGraph/index.js
@@ -14,10 +14,9 @@ import { compose } from 'redux';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import * as d3 from "d3";
-import { DragDropContext } from 'react-dnd';
+import * as d3 from 'd3';
 import MouseBackend from 'react-dnd-mouse-backend';
-import { DragSource, DropTarget } from 'react-dnd';
+import { DragSource, DropTarget, DragDropContext } from 'react-dnd';
 import { Button } from 'react-bootstrap';
 
 import { makeSelectActions } from '../App/selectors';
@@ -30,25 +29,23 @@ import saga from './saga';
 const knightSource = {
   beginDrag(props) {
     return props.action;
-  }
+  },
 };
 
 const linkSource = {
   beginDrag(props) {
-    console.log(props);
-    console.log([props.l.source.id, props.l.target.id]);
     return { sourceNodeId: props.l.source.id, targetNodeId: props.l.target.id };
-  }
+  },
 };
 
 const squareTarget = {
   drop(props, monitor) {
     const fromId = monitor.getItem().id;
     const toId = props.action.id;
-    if (fromId != toId) {
+    if (fromId !== toId) {
       props.dispatch(rest.actions.actions.addDependency(fromId, toId));
     }
-  }
+  },
 };
 
 const binTarget = {
@@ -57,81 +54,77 @@ const binTarget = {
     const fromId = getActionIdFromNodeId(monitor.getItem().sourceNodeId);
     const toId = getActionIdFromNodeId(monitor.getItem().targetNodeId);
     props.dispatch(rest.actions.actions.removeDependency(toId, fromId));
-  }
+  },
 };
 
-const collectDrag = (connect, monitor) => {
+const collectDrag = (connector, monitor) => {
   return {
-    connectDragSource: connect.dragSource(),
+    connectDragSource: connector.dragSource(),
     isDragging: monitor.isDragging(),
-  }
+  };
 };
 
-const collectDrop = (connect, monitor) => {
+const collectDrop = (connector, monitor) => {
   return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
-  }
+    connectDropTarget: connector.dropTarget(),
+    isOver: monitor.isOver(),
+  };
 };
 
 const radialPoint = (x, y) => {
   return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
 };
 
-const DeleteLink = DropTarget("LINK", binTarget, collectDrop)(({connectDropTarget, isOver}) => {
+const DeleteLink = DropTarget('LINK', binTarget, collectDrop)(({ connectDropTarget, isOver }) =>
   // Must wrap in div to satisfy DropTarget
-  return connectDropTarget(<circle r="20" style={ { fill: isOver ? '#990000' : '#000000' } } />);
-});
+  connectDropTarget(<circle r="20" style={{ fill: isOver ? '#990000' : '#000000' }} />)
+);
 
-const GraphNode = DropTarget("ACTION", squareTarget, collectDrop)(DragSource("ACTION", knightSource, collectDrag)(({d, action, onClick, connectDragSource, isDragging, connectDropTarget, isOver}) => {
-  return connectDropTarget(connectDragSource(
+const GraphNode = DropTarget('ACTION', squareTarget, collectDrop)(DragSource('ACTION', knightSource, collectDrag)(({ d, action, onClick, connectDragSource, isDragging, connectDropTarget, isOver }) =>
+  connectDropTarget(connectDragSource(
     <g
-      transform={'translate(' + radialPoint(d.x, d.y) + ')'}
-      style={ { fill: (isDragging ? '#cc00cc' : (isOver ? '#00cccc' : undefined)) } }
+      transform={`translate(${radialPoint(d.x, d.y)})`}
+      style={{ fill: (isDragging ? '#cc00cc' : (isOver ? '#00cccc' : undefined)) }}
       onClick={onClick}
     >
       <circle
         r="4"
-        style={ { fill: (d.children ? '#555555' : '#999999') } }
+        style={{ fill: (d.children ? '#555555' : '#999999') }}
       />
       <text
         dy="0.31em"
         x={d.x < Math.PI ? 6 : -6}
         textAnchor={d.x < Math.PI ? 'start' : 'end'}
-        transform={'rotate(' + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ')'}
+        transform={`'rotate(${(d.x < Math.PI ? d.x - (Math.PI / 2) : d.x + (Math.PI / 2)) * (180 / Math.PI)})`}
 
       >
         {action.shortDescription}
       </text>
     </g>
-  ));
-}));
+  ))
+));
 
 
-const GraphLink = DragSource("LINK", linkSource, collectDrag)(({l, connectDragSource, isDragging}) => {
-  return connectDragSource(
+const GraphLink = DragSource('LINK', linkSource, collectDrag)(({ l, connectDragSource, isDragging }) =>
+  connectDragSource(
     <path
-      style={ {
+      style={{
         fill: 'none',
         stroke: isDragging ? '#c0c' : '#bbb',
         strokeOpacity: 0.4,
         strokeWidth: '3px',
-      } }
-      d={d3.linkRadial().angle(function (d) {
-        return d.x;
-      }).radius(function (d) {
-        return d.y;
-      })(l)}
+      }}
+      d={d3.linkRadial().angle((d) => d.x).radius((d) => d.y)(l)}
     />
-  );
-});
+  )
+);
 
 class ActionGraph extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      selectedAction: null
-    }
+      selectedAction: null,
+    };
   }
 
   render() {
@@ -142,23 +135,23 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
     //   g = svg.append('g").attr("transform", );
 
     if (!actions.sync) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
 
-    var stratify = d3.stratify()
+    const stratify = d3.stratify()
       .id((d) => d.path)
-      .parentId(function(d) { return d.path.substring(0, d.path.lastIndexOf('.')); });
+      .parentId((d) => d.path.substring(0, d.path.lastIndexOf('.')));
 
-    var tree = d3.tree()
+    const tree = d3.tree()
       .size([2 * Math.PI, 1000])
-      .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+      .separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
 
     const depOns = {};
     Object.keys(actions.data).forEach((actionId) => {
       const action = actions.data[actionId];
       action.dependencies.forEach((dependencyId) => {
         if (!depOns[dependencyId]) {
-          depOns[dependencyId] = []
+          depOns[dependencyId] = [];
         }
         depOns[dependencyId].push(actionId);
       });
@@ -167,7 +160,7 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
     const filter = (id) => {
       const action = actions.data[id];
       return action.status === 0;
-    }
+    };
 
     const getDependencies = (path) => (id) => {
       if (!filter(id)) {
@@ -176,26 +169,30 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
       return [{ path: `${path}.${id}` }].concat(!depOns[id] ? [] : depOns[id].map((depId) => getDependencies(`${path}.${id}`)(depId)).reduce((acc, val) => acc.concat(val), []));
     };
 
-    const initial = selectedActionId ? { ...actions.data[selectedActionId], path: `${selectedActionId}` } : { path: 'start', shortDescription: 'WIN', };
+    const initial = selectedActionId ? { ...actions.data[selectedActionId], path: `${selectedActionId}` } : { path: 'start', shortDescription: 'WIN' };
     const other = selectedActionId ? (!depOns[selectedActionId] ? [] : depOns[selectedActionId]) : Object.keys(actions.data).filter((id) => actions.data[id].dependencies.length === 0);
 
-    var mapped = [initial].concat(other
+    const mapped = [initial].concat(other
       .filter((id) => {
         const action = actions.data[id];
-        return action.status === 0 && action.folder != 1; // todo other people's folders
+        return action.status === 0 && action.folder !== 1; // todo other people's folders
       })
-      .map((id) => {
-        return getDependencies(initial.path)(id);
-      })
+      .map((id) => getDependencies(initial.path)(id))
       .reduce((acc, val) => acc.concat(val), []));
 
-    var root = tree(stratify(mapped));
+    const root = tree(stratify(mapped));
 
     const renderNode = (d) => {
       const id = d.id.substring(d.id.lastIndexOf('.') + 1);
       const action = id === 'start' ? { shortDescription: 'WIN' } : actions.data[id];
       return [
-        <GraphNode key={d.id} d={d} action={action} dispatch={this.props.dispatch} onClick={() => this.setState({selectedAction: action})} />
+        <GraphNode
+          key={d.id}
+          d={d}
+          action={action}
+          dispatch={this.props.dispatch}
+          onClick={() => this.setState({ selectedAction: action })}
+        />,
       ].concat(d.children ? d.children.map(renderNode) : []);
     };
 
@@ -213,10 +210,10 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
             width="100%"
             height={1600}
             viewBox={`0 0 ${width} ${height}`}
-            style={{borderWidth:"2px", borderColor: "black", borderStyle: "solid"}}
+            style={{ borderWidth: '2px', borderColor: 'black', borderStyle: 'solid' }}
           >
             <DeleteLink dispatch={this.props.dispatch} />
-            <g transform={'translate(' + ((width / 2) + 40) + ',' + ((height / 2) + 90) + ')'}>
+            <g transform={`translate(${((width / 2) + 40)},${((height / 2) + 90)})`}>
               {renderLinks(root)};
               {renderNode(root)};
             </g>
@@ -227,16 +224,18 @@ class ActionGraph extends React.PureComponent { // eslint-disable-line react/pre
 }
 
 ActionGraph.propTypes = {
-  actions: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
+  selectedActionId: PropTypes.number,
 };
 
 const mapStateToProps = createStructuredSelector({
   actions: makeSelectActions(),
-  selectedActionId: (state) => state.get('selectedActionId')
+  selectedActionId: (state) => state.get('selectedActionId'),
 });
 
 function mapDispatchToProps(dispatch) {
-  return {dispatch};
+  return { dispatch };
 }
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
