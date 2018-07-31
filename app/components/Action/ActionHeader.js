2aadd8f3273c4f88006327810d11eda2e696d9f0
Jimbo Freedman <jimbo@project-sapiens.com>
2018-07-31 09:23:33 +0100

    Visual feedback for editing actions

--- app/components/Action/ActionHeader.js
+++ app/components/Action/ActionHeader.js
@@ -17,65 +17,79 @@ import DueLabel from './DueLabel';
 import config from '../../config';
 
 
-function ActionHeader(props) {
-  const { dispatch, action, folders, contexts } = props;
-  const loading = false;
+export class ActionHeader extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      finished: false
+    };
+  }
 
-  const handleHelper = (helperFunc) => (() => {
-    dispatch(helperFunc(action.id));
-  });
+  render() {
+    const { dispatch, action, folders, contexts } = this.props;
+    const { loading, finished } = this.state;
 
-  const handleUpdate = (actionDelta) => (() => {
-    dispatch(rest.actions.actions.put(
-      { id: action.id },
-      { body: JSON.stringify({ ...action, ...actionDelta }) }));
-  });
+    const handleHelper = (helperFunc) => (() => {
+      dispatch(helperFunc(action.id));
+    });
 
-  const menuItems = [
-    <MenuItem key="cancel" onClick={handleUpdate({ status: 2 })}><Glyphicon glyph="minus" />&nbsp;Cancel</MenuItem>,
-    <MenuItem key="fail" onClick={handleUpdate({ status: 1 })}><Glyphicon glyph="remove" />&nbsp;Fail</MenuItem>,
-    <MenuItem key="dfinish" divider />,
-    <MenuItem key={0} data={'some_data'} href={`${config.api.endpoint}admin/gtd/action/${action.id}/change`}>
-      API Edit
-    </MenuItem>,
-    <MenuItem key="dfolder" divider />,
-    <MenuItem key="hfolder" header>Move to folder</MenuItem>,
-    ...Object.keys(folders.data).map((id) => {
-      const folder = folders.data[id];
-      return (<MenuItem key={`folder${folder.id}`} onClick={handleUpdate({ folder: folder.id })}>{folder.name}</MenuItem>);
-    }),
-    <MenuItem key="dcontext" divider />,
-    <MenuItem key="hcontext" header>Change context</MenuItem>,
-    ...Object.keys(contexts.data).map((id) => {
-      const context = contexts.data[id];
-      return (<MenuItem key={`context${context.id}`} onClick={handleUpdate({ context: context.id })}>{context.name}</MenuItem>);
-    }),
-  ];
+    const handleUpdate = (actionDelta) => (() => {
+      dispatch(rest.actions.actions.put(
+        { id: action.id },
+        { body: JSON.stringify({ ...action, ...actionDelta }) }));
+      this.setState({ loading: true, finished: actionDelta.status > 0 });
+    });
+
+    const menuItems = [
+      <MenuItem key="cancel" disabled={loading} onClick={handleUpdate({ status: 2 })}><Glyphicon glyph="minus"/>&nbsp;Cancel</MenuItem>,
+      <MenuItem key="fail" disabled={loading} onClick={handleUpdate({ status: 1 })}><Glyphicon glyph="remove"/>&nbsp;Fail</MenuItem>,
+      <MenuItem key="dfinish" divider/>,
+      <MenuItem key={0} data={'some_data'} href={`${config.api.endpoint}admin/gtd/action/${action.id}/change`}>
+        API Edit
+      </MenuItem>,
+      <MenuItem key="dfolder" disabled={loading} divider/>,
+      <MenuItem key="hfolder" disabled={loading} header>Move to folder</MenuItem>,
+      ...Object.keys(folders.data).map((id) => {
+        const folder = folders.data[id];
+        return (
+          <MenuItem key={`folder${folder.id}`} disabled={loading} onClick={handleUpdate({ folder: folder.id })}>{folder.name}</MenuItem>);
+      }),
+      <MenuItem key="dcontext" divider/>,
+      <MenuItem key="hcontext" disabled={loading} header>Change context</MenuItem>,
+      ...Object.keys(contexts.data).map((id) => {
+        const context = contexts.data[id];
+        return (<MenuItem key={`context${context.id}`}
+                          onClick={handleUpdate({ context: context.id })}>{context.name}</MenuItem>);
+      }),
+    ];
 
 
-  const dueLabel = action.dueAt ? <DueLabel dueDate={action.dueAt} /> : null;
+    const dueLabel = action.dueAt ? <DueLabel dueDate={action.dueAt}/> : null;
 
-  return (
-    <Panel.Heading>
-      <div className="pull-right">
-        {dueLabel}
-        <ButtonGroup style={{ marginTop: '-5px' }}>
-          <SplitButton id={`dropdownMenu${action.id}`} bsSize="small" bsStyle="success" onClick={handleUpdate({ status: 3 })} title={<Glyphicon glyph="ok"/>} disabled={loading}>
-            {menuItems}
-          </SplitButton>
-        </ButtonGroup>
-      </div>
-      <div
-        style={{
-          textOverflow: 'ellipsis',
-          overflow: 'hidden',
-          whiteSpace: 'nowrap',
-        }}
-      >
-        <Glyphicon glyph={contexts.data[action.context].glyph} />
-        <Panel.Toggle componentClass="span">{action.shortDescription}</Panel.Toggle>
-      </div>
-    </Panel.Heading>);
+    return (
+      <Panel.Heading>
+        <div className="pull-right">
+          {dueLabel}
+          <ButtonGroup style={{ marginTop: '-5px' }}>
+            <SplitButton id={`dropdownMenu${action.id}`} bsSize="small" bsStyle="success"
+                         onClick={handleUpdate({ status: 3 })} title={<Glyphicon glyph="ok"/>} disabled={loading}>
+              {menuItems}
+            </SplitButton>
+          </ButtonGroup>
+        </div>
+        <div
+          style={{
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <Glyphicon glyph={contexts.data[action.context].glyph}/>
+          <Panel.Toggle componentClass="span" style={{ textDecoration: (finished ? 'line-through' : null) }}>{action.shortDescription}</Panel.Toggle>
+        </div>
+      </Panel.Heading>);
+  }
 }
 
 ActionHeader.propTypes = {
